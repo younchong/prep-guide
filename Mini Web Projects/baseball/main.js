a8055b1f104cae3c453741b94cdc05606a1e25bd
younchong <78673049+younchong@users.noreply.github.com>
2021-04-13 00:14:05 +0900

    NumberBaseball
    matching the number

--- Mini Web Projects/baseball/main.js
+++ Mini Web Projects/baseball/main.js
@@ -1,22 +1,55 @@
-function search(){
-let id= document.getElementById("getNumber").value;
-let board = document.getElementById("onBoard");
-let miss = [];
-miss.push(id);
-return  board.innerHTML=miss;
-}
 
+let list= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let ansArr= [];
+for(let i=0; i<3; i++){
+    let choice= list.splice(Math.random()*list.length, 1);
+ ansArr[i]=choice;
+}
 function makeNumber(){
-    let gang= [];
-    gang.push(Math.floor(Math.random()*10));
-    gang.push(Math.floor(Math.random()*10))
-    gang.push(Math.floor(Math.random()*10))
-    //gang.join
-    return console.log(gang);
+    for(let i=0; i<3; i++){
+        let choice= list.splice(Math.random()*list.length, 1);
+     ansArr[i]=parseInt(choice);
+    }
+    
+    return console.log(ansArr);
+    };
+
+let startBtn= document.getElementById("startBtn");
+startBtn.addEventListener("click", makeNumber);
+
+
+let count = 0;
+let giveNumberArr= [];
+function give(){
+    let numbers = document.getElementById("giveNumber");
+    let independence = numbers.value;
+    giveNumberArr = independence.split("");
+    console.log(giveNumberArr);
+    count++;
+    
+
+let strike = 0;
+let ball = 0;
+let out=0;
+    for(let i=0; i<3; i++){
+        for(let j=0; j<3; j++){
+            if(ansArr[i]==giveNumberArr[j] && i===j){
+                strike++;
+                if(strike===3){
+                console.error("win")}
+            }else if(ansArr[i]==giveNumberArr[j]){
+                ball++;
+                
+                }else if(ansArr[i]!=giveNumberArr[j] && i!==j){
+                    out++;
+                }
+            }
+            
+        }
+        console.error("count : ", count, "strike :", strike, "ball : ", ball, "out : ", out );
     }
 
 
-document.getElementsByClassName("onBoard").innerHTML="<div>right?</div>";  
+let giveNumberBtn = document.getElementById("giveNumberBtn");
+giveNumberBtn.addEventListener("click", give);
 
-let id = document.getElementById("btn");
-id.addEventListener("click", search);
