5ba34304de2f38ece7880077b1bba3b71f4b7411
younchong <78673049+younchong@users.noreply.github.com>
2021-04-13 19:51:04 +0900

    baseball
    almost finish
    without design

--- Mini Web Projects/baseball/main.js
+++ Mini Web Projects/baseball/main.js
@@ -6,6 +6,7 @@ for(let i=0; i<3; i++){
  ansArr[i]=choice;
 }
 function makeNumber(){
+    
     for(let i=0; i<3; i++){
         let choice= list.splice(Math.random()*list.length, 1);
      ansArr[i]=parseInt(choice);
@@ -17,39 +18,62 @@ function makeNumber(){
 let startBtn= document.getElementById("startBtn");
 startBtn.addEventListener("click", makeNumber);
 
+function winSound(){
+    let winSound= document.getElementById("winSound");
+    winSound.onplay;
+}
 
 let count = 0;
+let out=0;
 let giveNumberArr= [];
 function give(){
     let numbers = document.getElementById("giveNumber");
     let independence = numbers.value;
     giveNumberArr = independence.split("");
-    console.log(giveNumberArr);
     count++;
-    
+
+    if(independence.length!=3){
+        count--;
+        alert("3자리 숫자를 입력해주세요")
+    }
 
 let strike = 0;
 let ball = 0;
-let out=0;
+
+
+    if(count>10){
+        independence=null;
+        alert("over");
+        location.reload();
+    } 
     for(let i=0; i<3; i++){
         for(let j=0; j<3; j++){
             if(ansArr[i]==giveNumberArr[j] && i===j){
                 strike++;
                 if(strike===3){
-                console.error("win")}
+                alert("win")
+                winSound();
+                location.reload();}
             }else if(ansArr[i]==giveNumberArr[j]){
                 ball++;
                 
-                }else if(ansArr[i]!=giveNumberArr[j] && i!==j){
-                    out++;
                 }
             }
             
         }
-        console.error("count : ", count, "strike :", strike, "ball : ", ball, "out : ", out );
+        if(strike===0 && ball===0 && independence.length == 3){
+            out++;
+        }
+        
+        document.querySelector("#onBoard").innerHTML=`<div class=onBoard-status> ${independence} count: ${count}, strike: ${strike}, ball: ${ball}, out: ${out} </div>`
     }
 
 
 let giveNumberBtn = document.getElementById("giveNumberBtn");
 giveNumberBtn.addEventListener("click", give);
 
+function tryagain(){
+    location.reload();
+}
+
+document.querySelector("#tryAgain").addEventListener("click", tryagain);
